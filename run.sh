da4496c48a6503d957594335b9f94f79f6de3809
double_D <18374822143@163.com>
2021-09-08 15:06:50 +0800

    2021-09-08 15:06:50​

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ export MINIO_ACCESS_KEY=Minio
 export MINIO_SECRET_KEY=miniodoubled
 
 # 启动集群的命令中，必须列出集群中所有节点中的所有磁盘的URL
-go run /root/double_D/minio/main.go server http://172.17.0.2/root/res/miniodir http://172.17.0.2/root/res/miniodir \
-               http://172.17.0.3/root/res/miniodir http://172.17.0.3/root/res/miniodir \
-               http://172.17.0.4/root/res/miniodir http://172.17.0.4/root/res/miniodir --console-address ":9001"
+go run /root/double_D/minio/main.go server http://172.17.0.2/root/res/miniodir \
+               http://172.17.0.3/root/res/miniodir \
+               http://172.17.0.4/root/res/miniodir --console-address ":9001"
